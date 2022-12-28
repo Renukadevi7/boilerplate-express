3c76b406fd665cce1cab02b8d828fb8c4c72e99c
Renukadevi <j.renukadevi7@gmail.com>
2022-12-27 20:31:36 -0600

    updated path

--- myApp.js
+++ myApp.js
@@ -3,6 +3,18 @@ let app = express();
 
 
 
+//console.log("Hello World");
+
+// app.get("/", (req, res) => {
+//     res.send('Hello Express');
+// });
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+  module.exports = app;
 
 
 
