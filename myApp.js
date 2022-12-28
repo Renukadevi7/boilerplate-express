dfca4bd5eee719d419eba2884b1a698d45836675
Renukadevi <j.renukadevi7@gmail.com>
2022-12-27 20:52:49 -0600

    changed path

--- myApp.js
+++ myApp.js
@@ -11,7 +11,7 @@ let app = express();
 
 app.get('/', (req, res) => {
 
-  res.sendFile(__dirname + './views/index.html');
+  res.sendFile(__dirname + '/views/index.html');
 });
   module.exports = app;
 
