a2b45a33877e8b8b52675c488d6222eaae867e3d
Renukadevi <j.renukadevi7@gmail.com>
2022-12-28 22:33:46 -0600

    done challenge

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 let express = require("express");
 let app = express();
-let logger =('/logger.js');
 require("dotenv").config();
+const bodyParser = require('body-parser');
 
 //console.log("Hello World");
 
@@ -12,9 +12,12 @@ require("dotenv").config();
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.use( (req, res, next ) =>{
- console.log(req.method + " " + req.path + " - " + req.ip);
- next();
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
 });
 
 app.get("/", (req, res) => {
@@ -22,24 +25,40 @@ app.get("/", (req, res) => {
 });
 
 app.get("/json", (req, res) => {
- 
-  let greeting = 'Hello json';
+  let greeting = "Hello json";
 
-  if (process.env.MESSAGE_STYLE === 'uppercase') {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
     greeting = greeting.toUpperCase();
   }
-  res.json({ message : greeting });
+  res.json({ message: greeting });
 });
 
-app.get('/now', (req, res, next) => {
-  req.time = new Date().toString();
-  next();
- }, function( req , res){
-   res.send({time: req.time})
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  function (req, res) {
+    res.json({ time: req.time });
+  }
+);
+
+app.get("/:word/echo", (req, res) => {
+  const word = req.params.word;
+  res.json({ echo: word });
+});
+
+app.get("/name", (req, res) => {
+  //?first=firstname&last=lastname
+  const { first, last } = req.query;
+  res.json({ name: `${first} ${last}` });
 });
 
 
+app.post("/name", (req, res) => {
+  const { first, last } = req.body;
+  res.json({ name: `${first} ${last}` });
+});
 
 module.exports = app;
-
-
